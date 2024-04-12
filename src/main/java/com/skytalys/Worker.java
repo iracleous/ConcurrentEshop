500eb7097ddf1d478824ca0fd2d9eae2075c3faa
dimitris <iracleous@gmail.com>
2024-04-12 11:19:20 +0300

    Initial commit

---
+++ src/main/java/com/skytalys/Worker.java
@@ -0,0 +1,25 @@
+package com.skytalys;
+
+public class Worker implements Runnable {
+    private int id;
+
+    public Worker(int id) {
+        this.id = id;
+    }
+
+    public void doWork(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Worker " + id + " started");
+        doWork();
+        System.out.println("Thread " + id + " has finished working.") ;
+    }
+}
